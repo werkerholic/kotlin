9ddee739e99e4e62a539685cbaed47af5a4d4506
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-02-16 23:06:53 +0300

    Extract light analysis tests from box tests

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -237,6 +237,10 @@ fun main(args: Array<String>) {
             model("codegen/box", targetBackend = TargetBackend.JVM)
         }
 
+        testClass<AbstractLightAnalysisModeTest> {
+            model("codegen/box", targetBackend = TargetBackend.JVM)
+        }
+
         testClass<AbstractIrBlackBoxCodegenTest>("IrOnlyBoxCodegenTestGenerated") {
             model("ir/box", targetBackend = TargetBackend.JVM)
         }
