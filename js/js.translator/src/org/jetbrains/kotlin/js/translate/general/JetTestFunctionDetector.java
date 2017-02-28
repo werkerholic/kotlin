05c76e5d6aac6dd80559de6cd413becab42b7744
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-02-28 12:43:54 +0300

    JS: fix performance regression caused by changing approach to detection of main and test function from PSI-based to descriptor-based

--- js/js.translator/src/org/jetbrains/kotlin/js/translate/general/JetTestFunctionDetector.java
+++ js/js.translator/src/org/jetbrains/kotlin/js/translate/general/JetTestFunctionDetector.java
@@ -25,6 +25,7 @@ import org.jetbrains.kotlin.descriptors.annotations.AnnotationDescriptor;
 import org.jetbrains.kotlin.descriptors.annotations.Annotations;
 import org.jetbrains.kotlin.name.FqName;
 import org.jetbrains.kotlin.resolve.DescriptorUtils;
+import org.jetbrains.kotlin.resolve.scopes.DescriptorKindFilter;
 import org.jetbrains.kotlin.resolve.scopes.MemberScope;
 import org.jetbrains.kotlin.types.KotlinType;
 
@@ -73,7 +74,9 @@ public class JetTestFunctionDetector {
             @NotNull List<FunctionDescriptor> foundFunctions
     ) {
         for (PackageFragmentDescriptor packageDescriptor : moduleDescriptor.getPackage(packageName).getFragments()) {
-            Collection<DeclarationDescriptor> descriptors = DescriptorUtils.getAllDescriptors(packageDescriptor.getMemberScope());
+            if (DescriptorUtils.getContainingModule(packageDescriptor) != moduleDescriptor) continue;
+            Collection<DeclarationDescriptor> descriptors = packageDescriptor.getMemberScope().getContributedDescriptors(
+                    DescriptorKindFilter.CLASSIFIERS, MemberScope.Companion.getALL_NAME_FILTER());
             for (DeclarationDescriptor descriptor : descriptors) {
                 if (descriptor instanceof ClassDescriptor) {
                     getTestFunctions((ClassDescriptor) descriptor, foundFunctions);
@@ -86,14 +89,14 @@ public class JetTestFunctionDetector {
         }
     }
 
-    @NotNull
     private static void getTestFunctions(
             @NotNull ClassDescriptor classDescriptor,
             @NotNull List<FunctionDescriptor> foundFunctions
     ) {
         if (classDescriptor.getModality() == Modality.ABSTRACT) return;
 
-        Collection<DeclarationDescriptor> allDescriptors = DescriptorUtils.getAllDescriptors(classDescriptor.getUnsubstitutedMemberScope());
+        Collection<DeclarationDescriptor> allDescriptors = classDescriptor.getUnsubstitutedMemberScope().getContributedDescriptors(
+                DescriptorKindFilter.FUNCTIONS, MemberScope.Companion.getALL_NAME_FILTER());
         List<FunctionDescriptor> testFunctions = ContainerUtil.mapNotNull(
                 allDescriptors,
                 new Function<DeclarationDescriptor, FunctionDescriptor>() {
